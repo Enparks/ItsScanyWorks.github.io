6bc2269e483d2557cccea7a65975bd2de6195ce2
Brant <brant.marshall@gmail.com>
2016-08-01 20:48:59 -0400

    Complete rebrand
    Completely rebranded the main page. Visit itsbrant.com for demo.

--- README.md
+++
@@ -1,4 +0,0 @@
-# Brant's personal site.
-Brant's personal site. Feel free to download and use if wanted.
-
-SASS, SCSS, Jeykll, etc.
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>It's Brant! &raquo; Community & Social Media Professional</title>
+<meta name="author" content="Brant">
+<link href="assets/css/global.css" rel="stylesheet" media="all"/>
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" media="screen" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+    <div class="header">
+        <h1 class="title"> Hi! It's Brant</h1>
+        <p class="description">I'm a Community & Social Media Management Professional within the Twitch Community & a Twitch Support Volunteer.</p> <br/>
+        <p class="blog"> You can visit my blog by clicking <a href="http://blog.itsbrant.com" class="blog">here.</a></p>
+        <br/><br/><br/>
+        <a href="mailto:me@itsbrant.com?subject=Hey! I want to speak with you regarding..."><i class='fa fa-2x fa-send' title='me@itsbrant.com'></i></a> <a href="http://twitch.tv/hiitsbrant"><i class='fa fa-2x fa-twitch' title='TwitchTV'></i></a> <a href="http://twitter.com/hiitsbrant"><i class="fa fa-2x fa-twitter" title data-roriginal-title="Twitter"></i></a> <a href="http://github.com/hiitsbrant"><i class="fa fa-2x fa-github" title data-roriginal-title="GitHub"></i></a> <a href="http://reddit.com/hiitsbrant"><i class="fa fa-2x fa-reddit" title data-roriginal-title="Reddit"></i></a> 
+    </div>
+    
+</body>
+</html>
+
