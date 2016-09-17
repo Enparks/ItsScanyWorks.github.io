c9e375a079fd296f0b8aabfe33724c67f0f34364
Brant <brant.marshall@gmail.com>
2016-09-17 05:15:13 -0400

    Added new blog info.
    Updated Blog information. The blog is now hosted off site @ Medium and
    the anchor now reflects that.

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
     <div class="header">
         <h1 class="title"> Hi! It's Brant</h1>
         <p class="description">I'm a Community & Social Media Management Professional within the Twitch Community & a Twitch Support Volunteer.</p> <br/>
-        <p class="blog"> You can visit my blog by clicking <a href="http://blog.itsbrant.com" class="blog">here.</a></p>
+        <p class="blog"> My blog is proudly hosted by Medium! Visit it by clicking <a href="http://medium.com/@hiitsbrant" class="blog">here.</a></p>
         <br/><br/><br/>
-        <a href="mailto:me@itsbrant.com?subject=Hey! I want to speak with you regarding..."><i class='fa fa-2x fa-send' title='me@itsbrant.com'></i></a> <a href="http://twitch.tv/hiitsbrant"><i class='fa fa-2x fa-twitch' title='TwitchTV'></i></a> <a href="http://twitter.com/hiitsbrant"><i class="fa fa-2x fa-twitter" title data-roriginal-title="Twitter"></i></a> <a href="http://github.com/hiitsbrant"><i class="fa fa-2x fa-github" title data-roriginal-title="GitHub"></i></a> <a href="http://reddit.com/hiitsbrant"><i class="fa fa-2x fa-reddit" title data-roriginal-title="Reddit"></i></a> 
+        <a href="mailto:me@itsbrant.com?subject=Hey! I want to speak with you regarding..."><i class='fa fa-2x fa-send' title='me@itsbrant.com'></i></a> <a href="http://twitch.tv/hiitsbrant"><i class='fa fa-2x fa-twitch' title='TwitchTV'></i></a> <a href="http://twitter.com/hiitsbrant"><i class="fa fa-2x fa-twitter" title data-roriginal-title="Twitter"></i></a> <a href="http://github.com/hiitsbrant"><i class="fa fa-2x fa-github" title data-roriginal-title="GitHub"></i></a> <a href="https://www.reddit.com/user/HiItsBrant/"><i class="fa fa-2x fa-reddit" title data-roriginal-title="Reddit"></i></a> 
     </div>
     
 </body>
